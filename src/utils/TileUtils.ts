457885eaeb57ed0d46f559db6884e7e455f5ffbf
Karl Gunnarsson <karlgunnarsson98@gmail.com>
2024-03-02 13:56:20 +0000

    GH-6: implement function for getting empty tile coordinates

--- src/utils/TileUtils.ts
+++ src/utils/TileUtils.ts
@@ -1,7 +1,7 @@
 import { Board } from "../types/Board";
 
 export function getEmptyTileCoordinates(board: Board) {
-  console.log("Not implemented", board);
+  return { x: board.emptyTileRowIndex, y: board.emptyTileColumnIndex };
 }
 export function isValidMove(board: Board) {
   console.log("Not implemented", board);
